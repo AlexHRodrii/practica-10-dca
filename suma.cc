5bf84d41ef401d2e441cc5c0e51d14199c9f7f83
AlexHRodrii <ahr24@alu.ua.es>
2021-12-29 21:39:06 +0100

    Avanza el desarrollo

--- suma.cc
+++ suma.cc
@@ -4,9 +4,9 @@ int main()
   int num1;
   int num2;
   int resultado;
-  std::cout << "Ingrese el primer numero\n";
+  std::cout << "Ingrese el primer numero para la suma\n";
   std::cin >> num1;
-  std::cout << "Ingrese el segundo numero\n";
+  std::cout << "Ingrese el segundo numero para la suma\n";
   std::cin >> num2;
   resultado = num1 - num2;
   std::cout << resultado << "\n";
